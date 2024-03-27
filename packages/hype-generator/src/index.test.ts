dd9c7e2f2fa81ba1f087c67de3be9b92720cb690
Werner Stoltsz <werner.stoltsz@kerv.com>
2024-03-27 16:22:49 +0200

    feat: add greeter as dependency to hype

--- packages/hype-generator/src/index.test.ts
+++ packages/hype-generator/src/index.test.ts
@@ -1,7 +1,13 @@
 import { generateHype } from "./index";
+import * as  englishGreeterMock from 'greeter';
 
 describe("generateHype", () => {
   test("Says that everything is awesome", () => {
     expect(generateHype(3)).toBe("This is awesome!!!");
+  });
+
+  test("Should optionally prefix with a greeting", () => {
+    jest.spyOn(englishGreeterMock, 'englishGreeter').mockReturnValue('Hello Joe Soap')
+    expect(generateHype(3, {addressee: 'Joe Soap'})).toBe("Hello Joe Soap\nThis is awesome!!!");
   });
 });
--- packages/hype-generator/src/index.ts
+++ packages/hype-generator/src/index.ts
@@ -1,1 +1,13 @@
-export const generateHype = (levelOfHype: number) => `This is awesome${'!'.repeat(Math.abs(levelOfHype))}`;
+import {englishGreeter} from 'greeter';
+
+export const generateHype = (levelOfHype: number, options: {
+  addressee?: string,
+}) => {
+  const hype = `This is awesome${'!'.repeat(Math.abs(levelOfHype))}`;
+
+  if (options?.addressee){
+    return [englishGreeter(options.addressee), hype].join('\n');
+  }
+
+  return hype;
+}
