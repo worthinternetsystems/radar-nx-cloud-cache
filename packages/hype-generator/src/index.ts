bf9616fabcd1d2a28b3401e3c3c00c119a51fe78
Alex Clarke <alexander.clarke@kerv.com>
2024-03-27 14:45:05 +0000

    update package.json

--- packages/hype-generator/src/index.ts
+++ packages/hype-generator/src/index.ts
@@ -1,13 +1,16 @@
-import {englishGreeter} from 'greeter';
+import { englishGreeter } from "greeter";
 
-export const generateHype = (levelOfHype: number, options: {
-  addressee?: string,
-}) => {
-  const hype = `This is awesome${'!'.repeat(Math.abs(levelOfHype))}`;
+export const generateHype = (
+  levelOfHype: number,
+  options: {
+    addressee?: string;
+  }
+) => {
+  const hype = `This is TERRIBLE >:( ${"!".repeat(Math.abs(levelOfHype))}`;
 
-  if (options?.addressee){
-    return [englishGreeter(options.addressee), hype].join('\n');
+  if (options?.addressee) {
+    return [englishGreeter(options.addressee), hype].join("\n");
   }
 
   return hype;
-}
+};
