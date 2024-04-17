5d8fd925ccd42308177199400170bda30577a785
Chris Lelicanin <christian.lelicanin@kerv.com>
2024-04-17 11:46:19 +0200

    test cache

--- packages/manny-packagio/src/index.test.ts
+++ packages/manny-packagio/src/index.test.ts
@@ -1,6 +1,6 @@
 import { placeholder } from "./index";
 describe("placeholder", () => {
   test("placeholder text is correct", () => {
-    expect(placeholder).toBe("placeholder");
+    expect(placeholder).toBe("placeholder or something");
   });
 });
